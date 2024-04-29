69f737033bf8422570f6d1789432a8dd15c90ac0
keai <keai336wxy@gmail.com>
2024-04-29 12:56:48 +0800

    实现了一些更普遍的delay测试方法.

---
+++ delay_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/obgnail/clash-api/clash"
+)
+
+func GeneralDelayTest(name string) (*clash.ProxyDelay, error) {
+	return clash.GetProxyDelay(name, "http://www.gstatic.com/generate_204", 1000)
+}
+
+func GetALLDelayNow() map[string]int {
+	proxies, err := clash.GetProxies()
+	if err != nil {
+		panic("wrong get proxies")
+	}
+	delay_dic := make(map[string]int)
+	for k, _ := range proxies {
+		go func(k string) {
+			delay, _ := GeneralDelayTest(k)
+			delay_dic[k] = delay.Delay
+		}(k)
+	}
+	return delay_dic
+}
+
+//func TestAllDelay(t *testing.T) {
+//
+//	alldelay := AllDelayTest()
+//}
