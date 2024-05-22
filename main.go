b474c27a539d73eb7481e9dce0fe33c124ff7a26
keai <keai336wxy@gmail.com>
2024-05-22 19:52:49 +0800

    修正回收操作

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
-	casher := NewOneCasher("test.yaml")
-	defer casher.OffDuty()
-	for {
-		casher.Update()
-		time.Sleep(time.Duration(casher.Interval) * time.Second)
+	var path string = "config.yaml"
+	args := os.Args
+	if len(args) >= 2 {
+		path = args[1]
 	}
+	fmt.Println("path:", path)
+	casher := NewOneCasher(path)
+	go func() {
+		for {
+			casher.Update()
+			time.Sleep(time.Duration(casher.Interval) * time.Second)
+		}
+
+	}()
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+	casher.OffDuty()
+	os.Exit(1)
 }
